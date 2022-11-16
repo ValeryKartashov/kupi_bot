c5847bf193c82d7be797436554a1ffba03431012
DESKTOP-SU2DNI1\socdd <jacobcerf@gmail.com>
2022-11-16 22:35:49 +0400

    Добавление

--- Controllers/DatabaseController.cs
+++ Controllers/DatabaseController.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Bot.Models.ModelsDB;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace KupiCore.Controllers
 {
@@ -8,6 +9,22 @@ namespace KupiCore.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(string name, string description)
+        {
+            using (var db = new BotDbContext())
+            {
+                db.ChangeTracker.Clear();
+                db.ProductCategories.Add(new ProductCategory
+                {
+                    Name = name,
+                    Description = description
+                });
+                db.SaveChanges();
+                return View("~/Pages/Database/Index.cshtml");
+            }
         }
     }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,3 @@
-﻿
+﻿export function AddCategory(name) {
+    alert(`Hello, ${user}!`);
+}
